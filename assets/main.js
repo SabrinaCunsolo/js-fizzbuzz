711c10b45160ce882fa399564a130b46ffc84fc0
Sabrina <sabrinacunsolo85@gmail.com>
2020-10-20 14:52:55 +0200

    prima parte esercizio

--- assets/main.js
+++ assets/main.js
@@ -3,3 +3,14 @@
 // al posto dei multipli di 5, deve stampare "Buzz"
 // al posto dei multipli sia di 3 che di 5, deve stampare "FizzBuzz"
 // Nome repo: js-fizzbuzz
+
+// stampo i numeri da 1 a 100
+for (var i = 1; i <= 100; i++) {
+    // console.log(i);
+    document.write (i)
+    // se multiplo di 3 dovrò scrivere "Fizz"
+    if (i % 3 == 0) {
+        // console.log("Fizz")
+        document.write ("Fizz")
+    }
+}
